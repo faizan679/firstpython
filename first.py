2f71ccfa10d41170784d7a6e42336627f6c80ad0
FAIZAN KHAN <1605126@kiit.ac.in>
2018-02-08 20:51:31 +0530

    new addition

--- first.py
+++ first.py
@@ -1,6 +1,6 @@
 from datetime import datetime
-odds=[1,3,5,7,9,11,13,15]
-this_min=datetime.today().second
+odds=[1,3,5,7,9,11,13,15,17,19,21,23,25,27,29]
+this_min=datetime.today().minute
 if this_min in odds:
     print("This minute seems a lil' odd.")
 else:
